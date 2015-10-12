afdcda47c885097a5171eadd0a25df0c8d8cce00
Your Name <you@example.com>
2015-10-13 00:10:41 +0200

    text wrapped

--- cowsay.py
+++ cowsay.py
@@ -3,7 +3,7 @@ import textwrap
 class Cow:
     message = """
  ________________________________________
-/ %s \\
+/ %s\\
 %s\ %s/
  ----------------------------------------
         \   ^__^
@@ -17,7 +17,7 @@ class Cow:
         tmp = textwrap.wrap(tosay, 39)
 
         if len(tmp) == 1:
-            final = tmp[0] + ((39 - len(tmp[0])) * " ")
+            final = tmp[0] + ((40 - len(tmp[0])) * " ")
             mid = ''
             late = 39 * " "
         elif len(tmp) == 2:
@@ -26,8 +26,8 @@ class Cow:
             late = tmp[1] + ((40 - len(tmp[1])) * " ")
 
         else:
-            final = tmp[0]
+            final = tmp[0] + ((40 - len(tmp[0])) * " ")
             mid = "| " + " |\n| ".join([i + (39 - len(i)) * " " for i in tmp[1:-1]]) + " |\n"
-            late = tmp[1] + ((40 - len(tmp[1])) * " ")
+            late = tmp[-1] + ((40 - len(tmp[-1])) * " ")
 
         print(cls.message % (final, mid, late))
